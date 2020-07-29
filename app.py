41ec4175bac8f622441fe82a496836809cdd0cd3
adamwiraszka <32465914+adamwiraszka@users.noreply.github.com>
2020-07-29 01:14:33 -0400

    configured progress bar

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import downloader as ad
+import time
 import os
 import json
+import threading
 from tkinter import *
 from tkinter import ttk
 from ttkthemes import themed_tk as tk
@@ -10,10 +12,12 @@ from PIL import ImageTk, Image
 HEIGHT = 432
 WIDTH = 768
 
-# def main():
-#     root = Tk()
-#     window = App(root)
-#     return None
+display_text = {'searching': 'Searching for download links...',
+                'found': 'Found download links',
+                'choosing': 'Choosing best download link',
+                'tags': 'Applying media tags and cover art',
+                'finishing': 'Finishing up',
+                'done': 'Done'}
 
 class Window(tk.ThemedTk):
     def __init__(self):
@@ -25,7 +29,6 @@ class Window(tk.ThemedTk):
         self.maxsize(WIDTH, HEIGHT)
         self.iconphoto(False, PhotoImage(file='my_icon.png'))
         self.create_canvas()
-        #self.create_label()
         self.create_entry()
         self.create_button()
         #self.create_button_img()
@@ -33,19 +36,19 @@ class Window(tk.ThemedTk):
 
 
     def create_canvas(self):
-        canvas = Canvas(self, width=WIDTH, height=HEIGHT)
-        canvas.grid(column=0, row=0, rowspan=15, columnspan=5)
+        self.canvas = Canvas(self, width=WIDTH, height=HEIGHT)
+        self.canvas.grid(column=0, row=0, rowspan=15, columnspan=5)
         img_main = Image.open('bg_main.jpg')
         resized = img_main.resize((WIDTH, HEIGHT), Image.ANTIALIAS)
-        canvas.image = ImageTk.PhotoImage(resized)
-        canvas.create_image(0, 0, image=canvas.image, anchor='nw')
+        self.canvas.image = ImageTk.PhotoImage(resized)
+        self.canvas.create_image(0, 0, image=self.canvas.image, anchor='nw')
 
 
     def create_label(self):
         label_font = ('calibri', 10)
         label = ttk.Label(self, text='Enter a song:', anchor='w')
         label.config(font = label_font)
-        label.grid(column=0, row=12)
+        label.grid(column=2, row=15)
 
 
     def create_entry(self):
@@ -86,6 +89,27 @@ class Window(tk.ThemedTk):
         help_menu.add_command(label = 'About')
 
 
+    def create_progressbar(self):
+        self.progress_bar = ttk.Progressbar(self, orient = 'horizontal',
+                                            length = 250, mode = 'indeterminate')
+        self.progress_bar.grid(column=2, row=13, pady=10)
+        self.progress_bar['value'] = 0
+
+
+    def remove_widgets(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+
+    def switch_page(self):
+        if self.new_page == 'spotify_page':
+            print('spotify page')
+            self.spotify_page()
+        if self.new_page == 'download_page':
+            print('download page')
+            self.download_page()
+
+
     def refresh_window(self):
         self.destroy()
         self.__init__()
@@ -118,50 +142,225 @@ class Window(tk.ThemedTk):
         #print(self.search_results)
         print(self.search_status)
         if self.search_status:
-            self.display_results()
+            self.new_page = 'spotify_page'
+            self.remove_widgets()
+            self.switch_page()
         else:
             self.label = Label(self, text='Could not find track.')
             self.label.grid(column=2, row=14)
 
-    # NOT USED
-    def remove_widgets(self):
-        for widget in self.winfo_children():
-            widget.destroy()
 
-    # NOT USED
-    def switch_frame(self):
-        if self.new_page == 'spotify_page':
-            print('spotify page')
-            self.create_spotify_page()
-        elif self.new_page == 'download_page':
-            print('download page')
+    def spotify_page(self):
+        self.create_canvas()
+        self.create_entry()
+        self.create_menu()
+        self.create_button()
+        self.display_results()
 
 
     def display_results(self):
+        self.buttons = []
+        self.details = []
+        i = -1
         for track in self.search_results:
+            i += 1
             #print(track)
             id = track['id']
             artist = track['artist']
             name = track['track']
             album = track['album']
             duration = track['duration']
-            display_text = str(id) + ':' + artist + ' - ' + name + ' - ' + duration
-            button_info = Button(self, text=display_text, width=40)
-            button_info.grid(column=2)
-            print(display_text)
+            display_text = str(id) + ': ' + artist + ' - ' + name + ' - ' + duration
+            search_text = artist + ' - ' + name
+            self.details.append(search_text)
+            self.buttons.append(Button(self, text=display_text, width=40,
+                                       command = lambda i=i: self.choose_song(i)))
+            self.buttons[i].grid(column=3, row=i+3, sticky='w')
 
 
-    def create_progressbar(self):
-        self.progress_bar = ttk.Progressbar(self, orient = 'horizontal',
-                                            length = 180, mode = 'determinate')
-        self.progress_bar.grid(column=0, row=3, pady=10)
+    def choose_song(self, i):
+        self.index = i
+        print('user choice:', i)
+        self.search_str = self.details[i]
+        self.new_page = 'download_page'
+        self.remove_widgets()
+        self.switch_page()
 
+
+    def download_page(self):
+        self.create_canvas()
+        self.create_menu()
+        self.create_progressbar()
+        self.start_search_thread()
+
+
+    def start_search_thread(self):
+        self.show_text(display_text['searching'])
+        self.progress_part = 1
+        self.progress_rush = False
+        self.progress_updating = False
+        self.progress_thread = threading.Thread(target=self.progress).start()
+        self.search_thread = threading.Thread(target=self.yt_search)
+        self.search_thread.daemon = True
+        self.search_thread.start()
+        self.after(20, self.check_search_thread)
+
+
+    def check_search_thread(self):
+        if self.search_thread.is_alive():
+            self.after(20, self.check_search_thread)
+        else:
+            self.progress_rush = True
+            print('RUSH TO 25!')
+            self.compare_audio()
+
+
+    def compare_audio(self):
+        if self.progress_updating == True:
+            print('waiting')
+            self.after(80, self.compare_audio)
+        elif self.progress_updating == False:
+            self.progress_part = 2
+            self.show_text(display_text['choosing'])
+            self.progress_thread = threading.Thread(target=self.progress).start()
+            self.best_choice = ad.match_audio(self.search_str, self.search_results, self.youtube_results, self.index)
+            self.output_filename = str(self.best_choice[1])
+            self.after(900, self.start_dl_thread)  # pause so display text can be read
+
+
+    def start_dl_thread(self):
+        if self.progress_updating == True:
+            print('waiting')
+            self.after(80, self.start_dl_thread)
+        elif self.progress_updating == False:
+            dl_text = 'Downloading: ' + self.output_filename
+            self.show_text(dl_text)
+            self.progress_part = 3
+            self.progress_thread = threading.Thread(target=self.progress).start()
+            self.dl_thread = threading.Thread(target=self.yt_download)
+            self.dl_thread.daemon = True
+            self.dl_thread.start()
+            self.after(20, self.check_dl_thread)
+
+
+    def check_dl_thread(self):
+        if self.dl_thread.is_alive():
+            self.after(20, self.check_dl_thread)
+        else:
+            print('RUSH TO 85!')
+            self.progress_rush = True
+            self.apply_media_tags()
+
+
+    def apply_media_tags(self):
+        if self.progress_updating == True:
+            print('waiting')
+            self.after(80, self.apply_media_tags)
+        elif self.progress_updating == False:
+            self.progress_part = 4
+            self.progress_thread = threading.Thread(target=self.progress).start()
+            self.show_text(display_text['tags'])
+            ad.dl_cover_art(self.index, self.search_results)
+            ad.apply_ID3_tags(self.index, self.search_results, self.output_name, self.output_filename)
+            self.after(20, self.check_dl_complete)
+
+
+    def check_dl_complete(self):
+        if self.progress_thread.is_alive():
+            self.after(20, self.check_dl_thread)
+        else:
+            print('Download process complete')
+            self.show_text(display_text['done'])
+            self.final()
+
+
+    def final(self):
+        print('YAY')
+
+
+    def progress(self):
+        # Searching (0-25)
+        if self.progress_part == 1:
+            self.progress_updating = True
+            print('starting part 1')
+            for i in range(25):
+                current = i
+                print(current)
+                self.progress_bar['value'] = i
+                self.update_idletasks()
+                time.sleep(0.2)
+                if self.progress_rush:
+                    for n in range(current, 25):
+                        print(n)
+                        self.progress_bar['value'] = n
+                        self.update_idletasks()
+                        time.sleep(0.03)
+                    self.progress_rush = False
+                    break
+            self.progress_updating = False
+        # Choosing (25-35)
+        if self.progress_part == 2:
+            self.progress_updating = True
+            print('starting part 2')
+            for i in range(25,35):
+                print(i)
+                self.progress_bar['value'] = i
+                self.update_idletasks()
+                time.sleep(0.08)
+            self.progress_updating = False
+        # Downloading (35-85)
+        if self.progress_part == 3:
+            self.progress_updating = True
+            print('starting part 3')
+            for i in range(35,85):
+                current = i
+                print(current)
+                self.progress_bar['value'] = i
+                self.update_idletasks()
+                time.sleep(0.35)
+                if self.progress_rush:
+                    for n in range(current, 85):
+                        print(n)
+                        self.progress_bar['value'] = n
+                        self.update_idletasks()
+                        time.sleep(0.04)
+                    self.progress_rush = False
+                    break
+            self.progress_updating = False
+        # Applying media (85-100)
+        if self.progress_part == 4:
+            self.progress_updating = True
+            print('starting part 4')
+            for i in range(85,101):
+                print(i)
+                self.progress_bar['value'] = i
+                self.update_idletasks()
+                time.sleep(0.05)
+            self.progress_updating = False
+
+
+    def yt_search(self):
+        self.youtube_results = ad.search_youtube(self.search_str)
+        print(self.youtube_results)
+
+
+    def yt_download(self):
+        self.output_name = ad.dl_song(self.best_choice[0])
+        self.show_text(display_text['tags'])
+
+
+    def show_text(self, alpha_text):
+        try:
+            self.canvas.delete(self.canvas_text)
+        except:
+            pass
+        self.canvas_text = self.canvas.create_text(300,390, text = alpha_text, anchor='nw')
 
 
 
 
 class EntryWithPlaceholder(Entry):
-    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey'):
+    def __init__(self, master=None, placeholder='Enter song name', color='grey'):
         super().__init__(master)
 
         self.placeholder = placeholder
@@ -188,13 +387,6 @@ class EntryWithPlaceholder(Entry):
 
 
 
-
-window = Window()
-window.mainloop()
-
-
-
-
-# # Run application
-# if __name__ == "__main__":
-#main()
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
--- downloader.py
+++ downloader.py
@@ -25,7 +25,7 @@ def spotify_search(search_query):
 
     # Input song name and search spotify
     print('Searching for:', search_query)
-    result = spotify.search(search_query, limit=13)
+    result = spotify.search(search_query, limit=10)
     spotify_summary = None
     #print(json.loads(result, indent=2)
 
@@ -61,3 +61,149 @@ def spotify_search(search_query):
         spotify_summary = json.dumps(song_info, indent=2)
     #print(spotify_summary)
     return spotify_summary, search_success
+
+
+
+def search_youtube(search_str):
+# Youtube API Authentication and generate search request
+    youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+    request = youtube.search().list(q=search_str, part='snippet', type='video', maxResults=10)
+    results = request.execute()
+# Return video title and url for each result
+    youtube_results = []
+    for item in results['items']:
+        info = {}
+        info['vid_title'] = html.unescape(item['snippet']['title'])
+        info['url'] ='https://www.youtube.com/watch?v=' + item['id']['videoId']
+        #print('Analyzing:', info['vid_title'], info['url'])
+        # Get video durations and append to youtube_results
+        video_id = item['id']['videoId']
+        searchUrl = "https://www.googleapis.com/youtube/v3/videos?id="+video_id+"&key="+YOUTUBE_API_KEY+"&part=contentDetails"
+        response = urllib.request.urlopen(searchUrl).read()
+        data = json.loads(response)
+        duration = data['items'][0]['contentDetails']['duration']
+        duration = re.findall('\d+', duration)
+        info['duration_s'] = int(duration[0]) * 60
+        try:
+            info['duration_s'] = info['duration_s'] + int(duration[1])
+        except:
+            pass
+        info['duration'] = str(datetime.timedelta(seconds=info['duration_s'])).split('.')[0]
+        #print(info['duration'])
+        youtube_results.append(info)
+    return youtube_results
+
+
+def match_audio(search_str, spotify_summary, youtube_results, index):
+    search_success = True # THIS IS A PLACE HOLDER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    matched = False
+    vid_list = {
+        'best_vid' : '',
+        'best_url' : '',
+        'highest_match' : 0
+        }
+    for item in youtube_results:
+# Remove videos such as covers, live recordingss/performances unless explicitly asked for in search string
+        print('-' * 60)
+        print('Analyzing:', item['vid_title'], item['duration'])
+        if 'cover' not in search_str:
+            if 'cover' in item['vid_title'].lower():
+                print('DISALLOWED due to cover')
+                continue
+        if 'live' not in search_str:
+            if 'live' in item['vid_title'].lower():
+                print('DISALLOWED due to live')
+                continue
+# Score youtube results based on duration match
+        if search_success == True:
+            duration_diff = abs(spotify_summary[index]['duration_s'] - item['duration_s'])
+            duration_score = 0.35 * (100 - duration_diff) # perfect time match is 35 points
+            print('Duration score:', duration_score)
+
+# Use fuzzy matching to score similarity
+        match_ratio = fuzz.ratio(item['vid_title'], search_str)
+        print('Fuzzy score:', match_ratio)
+        if search_success == True:
+            match_score = match_ratio*0.65 + duration_score # perfect fuzzy match is 65 points
+        else:
+            match_score = match_ratio
+        print('Final matching score:', match_score)
+# Keep track of best youtube result in a dict
+        if match_score > vid_list['highest_match']:
+            vid_list['best_vid'] = item['vid_title']
+            vid_list['best_url'] = item['url']
+            vid_list['highest_match'] = match_score
+        #print('current best match is:', vid_list)
+        if match_score > 95:
+            matched = True
+            chosen_url = item['url']
+            chosen_video = item['vid_title']
+    if matched == False:
+        chosen_url = vid_list['best_url']
+        chosen_video = vid_list['best_vid']
+    print('=' * 60)
+    print('Chosen download link:', chosen_video)
+    print('=' * 60)
+    return chosen_url, chosen_video
+
+
+def dl_song(chosen_url):
+# Youtube_dl parameters config
+    download_options = {
+        'format': 'bestaudio/best',
+        'outtmpl': '%(title)s.%(ext)s',
+        'nocheckcertificate': 'True',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
+
+# Specify output directory for downloads
+    os.chdir('C:/Users/Adam/Desktop')
+    if not os.path.exists('songs'):
+        os.mkdir('songs')
+    os.chdir('songs')
+    #print('cwd is:', os.getcwd())
+
+# Download song from chosen Youtube url
+    with youtube_dl.YoutubeDL(download_options) as dl:
+        audio_filename = dl.prepare_filename(dl.extract_info(chosen_url))
+        audio_filename = audio_filename.replace('.m4a', '.mp3')
+        audio_filename = audio_filename.replace('.webm', '.mp3')
+        #dl.download([chosen_url])
+        print('filename is:', audio_filename)
+    return audio_filename
+
+
+def dl_cover_art(index, spotify_summary):
+# Download cover art
+    if os.path.exists('img.png'):
+        os.remove('img.png')
+    image_file = 'img.png'.format(0)
+    response = urllib.request.urlretrieve(spotify_summary[index]['album_art'], image_file)
+
+
+def apply_ID3_tags(index, spotify_summary, audio_filename, output_filename):
+# Rename audio file to 'temp.mp3' because file might have non-ascii characters (eyed3 cannot handle these)
+    try:
+        if os.path.exists('temp.mp3'):
+            os.remove('temp.mp3')
+            print('removed ')
+        os.rename(audio_filename, 'temp.mp3')
+        apply_tags = eyed3.load('temp.mp3')
+        apply_tags.initTag()
+        apply_tags.tag.images.set(3, open('img.png','rb').read(), 'image/png')
+        apply_tags.tag.artist = spotify_summary[index]['artist']
+        apply_tags.tag.title = spotify_summary[index]['track']
+        apply_tags.tag.album = spotify_summary[index]['album']
+        apply_tags.tag.album_artist = spotify_summary[index]['album_artist']
+        apply_tags.tag.recording_date = spotify_summary[index]['year']
+        apply_tags.tag.track_num = spotify_summary[index]['track_number']
+        apply_tags.tag.save()
+        print('Media tags and album art applied to audio file')
+        os.rename('temp.mp3', output_filename)
+    except:
+        print('Could not apply media tags to file')
+        os.chdir('../../')
